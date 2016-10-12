02f2cb9b83030ab1b75f13f1c173b7ead60fdc9a
Yenni Tan <Yenni_Tan@cable.comcast.com>
2016-10-12 12:14:51 -0400

    IAM Client Side Web browser
    IAM Client Side Web Browser Initial POC

---
+++ IAM/web/scripts/Home.js
@@ -0,0 +1,46 @@
+﻿
+function updateNameDisplay(event) {
+    $textMessage = document.getElementById('displayText').value + event
+    document.getElementById('displayText').value = $textMessage;
+    return $textMessage
+}
+
+
+function clearText() {
+    document.getElementById('displayText').value = ""
+    document.getElementById('keyInput').value = ""
+}
+
+//Update Text Value Message
+function textUpdate() {
+    var textValue = document.getElementsByName('textBox')[0].value;
+    var textString = AppendUnityCommand("ExecuteRequest", "HTML","HTMLGuiManager","UpdateText", textValue, "123456")
+    console.log(textString);
+    sendCommand(commandString);
+}
+
+//Get the Parameters and Append to a Unity Command    
+//Example: //'{"$types":{"ExecuteRequest, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null":"1"},"$type":"1","objectName":"HTML","objectType":"HTMLGuiManager","functionName":"UpdateText","functionParameters":["' + textValue + '"],"deviceId":"123456"}';
+//'{"$types":{"ExecuteRequest, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null":"1"},"$type":"1","objectName":"Wizard-Blue","objectType":"WizardController","functionName":"Run","functionParameters":[],"deviceId":"123456"}'
+function AppendUnityCommand(command, objectName, objectType, functionName, functionParameters, deviceId) {
+
+    console.log(functionParameters)
+    if (functionParameters == "") { console.log("Not Null") };
+
+    var functionalParameter = (functionParameters == "") ? "[]" : '["' + functionParameters + '"]'
+    console.log(functionalParameter)
+
+    var commandString = '{"$types":{"' +
+    command +
+    'Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null":"1"},"$type":"1","objectName":"' + objectName +
+    '","objectType":"' + objectType +
+    '","functionName":"' + functionName +
+    '","functionParameters":' + functionalParameter +
+    ',"deviceId":"' + deviceId + '"}';
+
+    console.log(commandString)
+    return commandString;
+
+}
+
+
---
+++ IAM/web/scripts/mqtt.js
@@ -0,0 +1,41 @@
+﻿
+    // called when the client connects
+    function onConnect() {
+        // Once a connection has been made, make a subscription and send a message.
+        console.log("onConnect");
+        client.subscribe("control/123456/request");
+    }
+
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("control/123456/request");
+  }
+
+  function doFail(e){
+    alert("failure: " + e);
+    console.log(e);
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      alert("connection lost: " + responseObject.errorMessage);
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+  }
+  
+  //send a command to the remote client
+  function sendCommand(commandString) {
+    message = new Paho.MQTT.Message(commandString);
+    message.destinationName = "control/123456/request";
+    client.send(message); 
+  }
+ 
+
