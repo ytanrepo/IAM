8192319cd24573e5de941c52b0adbc034632339b
Yenni Tan <Yenni_Tan@cable.comcast.com>
2016-10-17 17:28:40 -0400

    changes for supporting the enter key press
    changes for supporting the enter key press
    Fix the Line wrap
    Fix the Line wrap
    checkin
    checkin
    Add to repository
    Add to repository
    Changes to IAM Scripts

--- src/web/scripts/Home.js
+++ src/web/scripts/Home.js
@@ -1,82 +1,22 @@
 ﻿var client;
 
-function updateNameDisplay(event) {
-    $textMessage = document.getElementById('displayText').value + event
-    document.getElementById('displayText').value = $textMessage;
-    return $textMessage
-}
-
 
 function clearText() {
-    document.getElementById('displayText').value = ""
-    document.getElementById('keyInput').value = ""
+    document.getElementsByName('textBox')[0].value = '';
+    setUid('');
 }
 
-function InitializedMqtt(){
 
-    // Create a client instance
-    client = new Paho.MQTT.Client("m12.cloudmqtt.com", 34616, "web_" + parseInt(Math.random() * 100, 10));
 
-    // set callback handlers
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
-
-    var options = {
-        useSSL: true,
-        userName: "mike",
-        password: "password",
-        onSuccess: onConnect,
-        onFailure: doFail
-    }
-
-    // connect the client
-    client.connect(options);
-}
-// called when the client connects
-function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("onConnect");
-    client.subscribe("control/123456/request");
-}
-
-function doFail(e) {
-    alert("failure: " + e);
-    console.log(e);
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        alert("connection lost: " + responseObject.errorMessage);
-        console.log("onConnectionLost:" + responseObject.errorMessage);
-    }
-}
-
-//send a command to the remote client
-function sendCommand(commandString) {
-    message = new Paho.MQTT.Message(commandString);
-    message.destinationName = "control/123456/request";
-    client.send(message);
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-    console.log("onMessageArrived:" + message.payloadString);
-}
-
-function InitializeWizardController(functionParameters) {
-    var commandExecuted = SetupInitializedUnityCommand("ExecuteRequest", "Wizard-Blue", "WizardController", functionParameters, "", "123456")
+function initializeWizardController(functionParameters) {
+    var commandExecuted = setupInitializedUnityCommand("ExecuteRequest", "Wizard-Blue", "WizardController", functionParameters, "", "123456")
     return false;
 }
 
-//Get the Parameters and Append to a Unity Command
-//Example: //'{"$types":{"ExecuteRequest, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null":"1"},"$type":"1","objectName":"HTML","objectType":"HTMLGuiManager","functionName":"UpdateText","functionParameters":["' + textValue + '"],"deviceId":"123456"}';
-//'{"$types":{"ExecuteRequest, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null":"1"},"$type":"1","objectName":"Wizard-Blue","objectType":"WizardController",
-//"functionName":"Run","functionParameters":[],"deviceId":"123456"}'
-function SetupInitializedUnityCommand(command, objectName, objectType, functionName, functionParameters, deviceId) {
+function setupInitializedUnityCommand(command, objectName, objectType, functionName, functionParameters, deviceId) {
     var paramArray = [];
-    if (functionParameters.length > 0) {
-        paramArray.push(functionParameters);
+    if (Array.isArray(functionParameters) && functionParameters.length > 0) {
+        paramArray = functionParameters;
     }
 
     var result = {
@@ -95,11 +35,50 @@ function SetupInitializedUnityCommand(command, objectName, objectType, functionN
 }
 //Update Text Value Message
 function textUpdate() {
-    var textValue = document.getElementsByName('textBox')[0].value;
-    var textString = SetupInitializedUnityCommand("ExecuteRequest", "HTML", "HTMLGuiManager", "UpdateText", textValue, "123456")
+    var textValue = [getUid(), getText()];
+    if (textValue != null && textValue.length > 0) {
+        var textString = setupInitializedUnityCommand("ExecuteRequest", "HTML", "HTMLGuiManager", "UpdateText", textValue, "123456")
+    }
+    keyPressEscapeCheck();
 }
 
+function getUid() {
+    var hidden = document.getElementById('uid');
+    return hidden.value;
+}
+function setUid(setValue) {
+    document.getElementById('uid').value = setValue;
+}
 
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+}
 
+function getText() {
+    return document.getElementsByName('textBox')[0].value;
 
+}
+function initUid() {
+    var hidden = document.getElementById('uid');
+    hidden.setAttribute("value", guid());
+}
 
+function keyPressEscapeCheck() {
+    var key = window.event.keyCode;
+    var textValue = getText();
+    if (key == 13) {
+        sendKeywordAction();
+        clearText();
+    }
+}
+
+function sendKeywordAction() {
+    //var file = $.getJSon("scripts/keywordAction.json")
+    initializeWizardController('Walk');
+}
---
+++ src/web/scripts/mqttClient.js
@@ -0,0 +1,61 @@
+﻿ 
+  function initializedMqtt() {
+
+      // Create a client instance
+      client = new Paho.MQTT.Client("m13.cloudmqtt.com", 35424, "web_" + parseInt(Math.random() * 100, 10));
+
+      // set callback handlers
+      client.onConnectionLost = onConnectionLost;
+      client.onMessageArrived = onMessageArrived;
+
+      var options = {
+          useSSL: true,
+          userName: "web",
+          password: "password",
+          onSuccess: onConnect,
+          onFailure: doFail
+      }
+
+      // connect the client
+      client.connect(options);
+  }
+  // called when the client connects
+  function onConnect() {
+      // Once a connection has been made, make a subscription and send a message.
+      console.log("onConnect");
+      client.subscribe("control/123456/request");
+  }
+
+  function doFail(e) {
+      alert("failure: " + e);
+      console.log(e);
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+      if (responseObject.errorCode !== 0) {
+          alert("connection lost: " + responseObject.errorMessage);
+          console.log("onConnectionLost:" + responseObject.errorMessage);
+      }
+  }
+
+  //send a command to the remote client
+  function sendCommand(commandString) {
+      message = new Paho.MQTT.Message(commandString);
+      message.destinationName = "control/123456/request";
+      client.send(message);
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+      console.log("onMessageArrived:" + message.payloadString);
+  }
+
+  // called when the client connects
+  function onConnect() {
+      // Once a connection has been made, make a subscription and send a message.
+      console.log("onConnect");
+      client.subscribe("control/123456/request");
+  }
+
+
