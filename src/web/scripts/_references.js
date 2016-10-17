397cd08fa34b918a4d220a0acd5d4b6a7988425d
Bissol, Joseph <joseph_bissol@cable.comcast.com>
2016-10-17 17:41:57 -0400

    Add bootstrap

---
+++ src/web/scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="Home.js" />
+/// <reference path="knockout-3.4.0.js" />
+/// <reference path="mqtt.js" />
+/// <reference path="mqttws31.js" />
---
+++ src/web/scripts/app.js
@@ -0,0 +1,14 @@
+﻿// Build namespace if it does not already exist.
+var comcast = comcast || {};
+
+(function (namespace, document) {
+    /**
+     * Create IAM module.
+     */
+    namespace.iam = (function () {
+        "use strict";
+
+
+    });
+
+})(comcast.iam, document)
